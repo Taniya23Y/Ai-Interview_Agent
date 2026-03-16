99be5f1d49b397950b3497fb8f184edfb0a2c201
Taniya Yadav <128471154+Taniya23Y@users.noreply.github.com>
2026-03-16 13:14:17 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import InterviewPage from "./pages/InterviewPage.jsx";
 import { Route, Routes } from "react-router-dom";
